629ffe4a000de2d499c7912c8bfb49e1b429997b
sijia zhu <sijiazhu@sijias-iMac.local>
2018-07-15 01:41:44 -0700

    first commit

---
+++ src/test/java/com/example/finalprojectserversprintbootsummer2018/finalprojectTests.java
@@ -0,0 +1,16 @@
+package com.example.finalprojectserversprintbootsummer2018;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class finalprojectTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
